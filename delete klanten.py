b0a1edc6e9baf270ebb78f330060261215d4f363
david-warris <davidwarris@outlook.com>
2023-04-07 12:36:06 +0200

    infohuis gestyled, en andere upgrades
    ook een deleteboekingen functietoegevoegd aan de delete klanten file

--- delete klanten.py
+++ delete klanten.py
@@ -8,5 +8,14 @@ def deleteklanten():
             db.session.delete(cursist_elsje)
             db.session.commit()
 
+def deleteboekingen():
+    with app.app_context():
+        for i in range(len(Boekingen.query.all())):
+            print(f'deleted {i+1}')
+            cursist_elsje = Boekingen.query.get(i+1)
+            db.session.delete(cursist_elsje)
+            db.session.commit()
+
 if __name__=='__main__':
     deleteklanten()
+    deleteboekingen()
